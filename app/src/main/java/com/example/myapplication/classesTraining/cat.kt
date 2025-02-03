316f0168ab1f95a53ee04be7208278ce514e6cbb
Dmikko <johanson.mike@hotmail.com>
2025-02-03 10:05:30 +0100

    New package for classes

---
+++ app/src/main/java/com/example/myapplication/classesTraining/cat.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.classesTraining
+
+class Cat(
+    val sex: String,
+    var age: Int,
+    val color: String,
+    val breed: String,
+) {
+
+}
---
+++ app/src/main/java/com/example/myapplication/classesTraining/main.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.classesTraining
+
+fun main(){
+
+
+val cat = Cat("Male",2,"Tabby","House")
+println(cat)
+}
+
